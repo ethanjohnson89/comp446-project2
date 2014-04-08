db13c988efe8ca2a040ef0ca20df23e307ebff06
korykappel <kappelkr1@gcc.edu>
2014-04-08 03:55:47 -0400

    Levels and states

--- Crate/Crate.cpp
+++ Crate/Crate.cpp
@@ -38,6 +38,7 @@ public:
 	void drawScene(); 
 
 	void regenerateWalls(float dt);
+	void reinitialize();
 
 private:
 	void buildFX();
@@ -100,10 +101,16 @@ private:
 	float laserTimer;
 
 	int bossHealth;
+	float playerHealth;
 
 	bool spacePressedLastFrame;
 
 	int level;
+
+	State state;
+
+	float bossDyingTimer;
+	bool bossDead;
 
 	// DEBUG
 	Mesh mesh;
@@ -130,7 +137,7 @@ CrateApp::CrateApp(HINSTANCE hInstance)
   mfxLightVar(0), mfxDiffuseMapVar(0), mfxSpecMapVar(0), mfxTexMtxVar(0), 
   mVertexLayout(0), mDiffuseMapRV(0), mSpecMapRV(0), mEyePos(0.0f, 0.0f, 0.0f), 
   mRadius(25.0f), mTheta(0.0f), mPhi(PI/2), spinAmount(0), fireLaser(false), laserTimer(0), bossHealth(3),
-  spacePressedLastFrame(false), level(1)
+  spacePressedLastFrame(false), level(1), bossDyingTimer(0), bossDead(false)
 {
 	D3DXMatrixIdentity(&mCrateWorld);
 	D3DXMatrixIdentity(&mView);
@@ -139,6 +146,8 @@ CrateApp::CrateApp(HINSTANCE hInstance)
 
 	laserTheta = mTheta;
 	laserPhi = mPhi;
+
+	state = start;
 }
 
 CrateApp::~CrateApp()
@@ -242,186 +251,261 @@ void CrateApp::onResize()
 	D3DXMatrixPerspectiveFovLH(&mProj, 0.25f*PI, aspect, 1.0f, 1000.0f);
 }
 
+void CrateApp::reinitialize()
+{
+       spacePressedLastFrame = false;
+		//reset: player health, boss health, phis and thetas of laser and cam, setactive all walls, set their timers all to zero, 
+	   //set laser to inactive and its timer to 0 (actually set mPhi to PI/2)
+
+	   //if(level==1)
+
+	   //if(level==2)
+}
+
 void CrateApp::updateScene(float dt)
 {
 	D3DApp::updateScene(dt);
 
-	// DEBUG
-	mesh.update(dt);
+	switch(state)
+	{
+	case start:
 
-	// Update angles based on input to orbit camera around scene.
-	if(GetAsyncKeyState('A') & 0x8000)	mTheta += 3.0f*dt;
-	if(GetAsyncKeyState('D') & 0x8000)	mTheta -= 3.0f*dt;
-	if(GetAsyncKeyState('W') & 0x8000)	mPhi -= 3.0f*dt;
-	if(GetAsyncKeyState('S') & 0x8000)	mPhi += 3.0f*dt;
-	if(GetAsyncKeyState('Z') & 0x8000)	mRadius -= 15.0f*dt;
-	if(GetAsyncKeyState('X') & 0x8000)	mRadius += 15.0f*dt;
+		//UPDATE THE INTRO SPLASHSCREEN CUBE
 
-	for(int i=0; i<3; i++)
-		health[0].update(dt);
+		if(GetAsyncKeyState(VK_RETURN) & 0x8000) 
+		{
+			state = game;
+		}
+
+		break;
+
+	case restart:
+		//UPDATE THE RESTART SCREEN CUBE
+		if(GetAsyncKeyState(VK_RETURN) & 0x8000) 
+		{
+			state = game;
+			reinitialize();
+		}
+
+		break;
+
+	case game:
+	{
+
+		// DEBUG
+		mesh.update(dt);
+
+		// Update angles based on input to orbit camera around scene.
+		if(GetAsyncKeyState('A') & 0x8000)	mTheta += 3.0f*dt;
+		if(GetAsyncKeyState('D') & 0x8000)	mTheta -= 3.0f*dt;
+		if(GetAsyncKeyState('W') & 0x8000)	mPhi -= 3.0f*dt;
+		if(GetAsyncKeyState('S') & 0x8000)	mPhi += 3.0f*dt;
+		if(GetAsyncKeyState('Z') & 0x8000)	mRadius -= 15.0f*dt;
+		if(GetAsyncKeyState('X') & 0x8000)	mRadius += 15.0f*dt;
+
+		for(int i=0; i<3; i++)
+			health[0].update(dt);
 	
 
-	if(!spacePressedLastFrame && !bulletObject.getActiveState() && GetAsyncKeyState(' ') & 0x8000)
-	{
-		//if(!bullet1[0].getActiveState())
-		//{
-			//for(int i=0; i<3; i++)
+		if(!spacePressedLastFrame && !bulletObject.getActiveState() && GetAsyncKeyState(' ') & 0x8000)
+		{
+			//if(!bullet1[0].getActiveState())
 			//{
-			//	bullet1[i].setActive();
-			//	bullet1[i].setPosition(mEyePos);
-			//	//calculate velocity vector
-			//	Vector3 t(-mEyePos);
-			//	bullet1[i].setVelocity(t*2);
-			//}
-	//	}
-		bulletObject.shoot(mEyePos/2,2*Vector3(-mEyePos), mTheta, mPhi);
-	}
-
-	if(GetAsyncKeyState(' ') & 0x8000) spacePressedLastFrame = true;
-	else spacePressedLastFrame = false;
-	
-	spinAmount += 1*dt;
-	if (spinAmount*10 > 360)
-		spinAmount = 0;
-
-	//UPDATE LAYERS:
-	for(int i=0; i<5; i++)
-	{
-		//if(level == 1)
-		//	layers[i].updateMatrices(0);
-		//else if(level == 2)
-			layers[i].updateMatrices(spinAmount);
-	}
-	for(int i=0; i<NUM_LAYERS; i++)
-	{
-		for(int j=0; j<NUM_WALLS; j++)
-		{
-			layers[i].walls[j].update(dt);
-			/*if(bulletObject.collided(&layers[i].walls[j]))
-			{
-				layers[i].walls[j].setInActive();
-			}*/
+				//for(int i=0; i<3; i++)
+				//{
+				//	bullet1[i].setActive();
+				//	bullet1[i].setPosition(mEyePos);
+				//	//calculate velocity vector
+				//	Vector3 t(-mEyePos);
+				//	bullet1[i].setVelocity(t*2);
+				//}
+		//	}
+			bulletObject.shoot(mEyePos/2,2*Vector3(-mEyePos), mTheta, mPhi);
 		}
-	}
 
+		if(GetAsyncKeyState(' ') & 0x8000) spacePressedLastFrame = true;
+		else spacePressedLastFrame = false;
+	
+		spinAmount += 1*dt;
+		if (spinAmount*10 > 360)
+			spinAmount = 0;
 
-	//for(int i=0; i<3; i++)
-	//	bullet1[i].update(dt);
-	bulletObject.update(dt);
-	boss.update(dt);
-
-	//update once
-	for(int i=0; i<6; i++)
-		background[i].update(dt);
-
-	//BULLET COLLISION ON BOSS
-	/*if(bulletObject.collided(&boss))
-	{
-		bossHealth--;
-		if(bossHealth == 0) boss.setInActive();*/
-	if(bulletObject.collided(&mesh))
-	{
-		bossHealth--;
-		if(bossHealth == 0)
-			mesh.setInActive();
-		bulletObject.setInActive();
-	}
-
-	//BULLET COLLISION ON WALLS
-	if(bulletObject.getActiveState())
-	{
-		for(int j=0; j<NUM_LAYERS; j++)
+		//UPDATE LAYERS:
+		for(int i=0; i<5; i++)
 		{
-			for(int i=0; i<NUM_WALLS; i++)
+			//if(level == 1)
+			//	layers[i].updateMatrices(0);
+			//else if(level == 2)
+				layers[i].updateMatrices(spinAmount);
+		}
+		for(int i=0; i<NUM_LAYERS; i++)
+		{
+			for(int j=0; j<NUM_WALLS; j++)
 			{
-				if(layers[j].walls[i].getActiveState() && ((abs(bulletObject.getTheta() - layers[j].thetas[i]) < .3) || (abs(bulletObject.getTheta() - 2*PI - layers[j].thetas[i]) < .3)) && (abs(bulletObject.getPhi() - layers[j].phis[i]) < .3) && (abs(bulletObject.getDistanceToOrigin() - layers[j].radius) < .5))
+				layers[i].walls[j].update(dt);
+				/*if(bulletObject.collided(&layers[i].walls[j]))
 				{
-					layers[j].walls[i].setInActive();
-					bulletObject.setInActive();
-				}
+					layers[i].walls[j].setInActive();
+				}*/
 			}
 		}
-	}
 
 
-	//LASER COLLISION ON WALLS
-	if(fireLaser)
-	{
-		for(int j=0; j<NUM_LAYERS; j++)
+		//for(int i=0; i<3; i++)
+		//	bullet1[i].update(dt);
+		bulletObject.update(dt);
+		boss.update(dt);
+
+		//update once
+		for(int i=0; i<6; i++)
+			background[i].update(dt);
+
+		//BULLET COLLISION ON BOSS
+		/*if(bulletObject.collided(&boss))
 		{
-			for(int i=0; i<NUM_WALLS; i++)
+			bossHealth--;
+			if(bossHealth == 0) boss.setInActive();*/
+		if(bulletObject.collided(&mesh))
+		{
+			bossHealth--;
+			if(bossHealth == 0)
+				mesh.setInActive();
+			bulletObject.setInActive();
+			//PLAY SOUND
+			bossDead = true;
+		}
+		//WIN 
+		if(bossDead)
+		{
+			bossDyingTimer += dt;
+			if(bossDyingTimer > 3) //CHANGE TO LENGTH OF SOUND
 			{
-				if(layers[j].walls[i].getActiveState())
+				if(level==1) 
 				{
-					//if laserTheta is close to either 0 or 2PI, ignore theta comparisons
-					if(laserPhi < .3 || laserPhi > 2.7)
+					level = 2;
+					state = game;
+					reinitialize();
+				}
+				else if(level==2)
+					state = end;
+			}
+		}
+
+		//LOSE
+		//if(health <= 0) state = restart;
+
+
+		//BULLET COLLISION ON WALLS
+		if(bulletObject.getActiveState())
+		{
+			for(int j=0; j<NUM_LAYERS; j++)
+			{
+				for(int i=0; i<NUM_WALLS; i++)
+				{
+					if(layers[j].walls[i].getActiveState() && ((abs(bulletObject.getTheta() - layers[j].thetas[i]) < .3) || (abs(bulletObject.getTheta() - 2*PI - layers[j].thetas[i]) < .3)) && (abs(bulletObject.getPhi() - layers[j].phis[i]) < .3) && (abs(bulletObject.getDistanceToOrigin() - layers[j].radius) < .5))
 					{
-						if(abs(laserPhi - layers[j].phis[i]) < .3)
-						{
-							layers[j].walls[i].setInActive();
-						}
-					}
-					else
-					{
-						if((abs(laserTheta - layers[j].thetas[i]) < .3) && (abs(laserPhi - layers[j].phis[i]) < .3))
-						//if(layers[j].walls[i].getActiveState() && ((layers[j].thetas[i] > laserTheta) && (laserTheta + .3 > layers[j].thetas[i])) || ((layers[j].thetas[i] < laserTheta) && (layers[j].thetas[i] + .3 > laserTheta)))
-						{
-							layers[j].walls[i].setInActive();
-						}
+						layers[j].walls[i].setInActive();
+						bulletObject.setInActive();
 					}
 				}
 			}
 		}
-	}
 
-	
-	regenerateWalls(dt);
 
-	//CONTROLS WHEN LASER DISPLAYS
-	if(!fireLaser)
-	{
-		if(rand()%4000==0)
+		//LASER COLLISION ON WALLS
+		if(fireLaser)
 		{
-			fireLaser = true;
-			laser.setActive();
-			laserTheta = (int)(mTheta+PI)%6;
+			for(int j=0; j<NUM_LAYERS; j++)
+			{
+				for(int i=0; i<NUM_WALLS; i++)
+				{
+					if(layers[j].walls[i].getActiveState())
+					{
+						//if laserTheta is close to either 0 or 2PI, ignore theta comparisons
+						if(laserPhi < .3 || laserPhi > 2.7)
+						{
+							if(abs(laserPhi - layers[j].phis[i]) < .3)
+							{
+								layers[j].walls[i].setInActive();
+							}
+						}
+						else
+						{
+							if((abs(laserTheta - layers[j].thetas[i]) < .3) && (abs(laserPhi - layers[j].phis[i]) < .3))
+							//if(layers[j].walls[i].getActiveState() && ((layers[j].thetas[i] > laserTheta) && (laserTheta + .3 > layers[j].thetas[i])) || ((layers[j].thetas[i] < laserTheta) && (layers[j].thetas[i] + .3 > laserTheta)))
+							{
+								layers[j].walls[i].setInActive();
+							}
+						}
+					}
+				}
+			}
 		}
-	}
-	else
-	{
-		laser.update(dt);
-		laserTimer += dt;
-		//at end of timer,set timer back to zero, set laser inactive
-		if(laserTimer > 100)
+
+	
+		regenerateWalls(dt);
+
+		//CONTROLS WHEN LASER DISPLAYS
+		if(!fireLaser)
 		{
-			fireLaser = false;
-			laserTimer = 0;
-			laser.setInActive();
+			if(rand()%4000==0)
+			{
+				fireLaser = true;
+				laser.setActive();
+				laserTheta = (int)(mTheta+PI)%6;
+			}
 		}
+		else
+		{
+			laser.update(dt);
+			laserTimer += dt;
+			//at end of timer,set timer back to zero, set laser inactive
+			if(laserTimer > 100)
+			{
+				fireLaser = false;
+				laserTimer = 0;
+				laser.setInActive();
+			}
+		}
+
+
+	
+		// Restrict the angle mPhi.
+		if( mPhi < 0.1f )	mPhi = 0.1f;
+		if( mPhi > PI-0.1f)	mPhi = PI-0.1f;
+
+		if( mTheta < 0.01f )	mTheta = 2*PI-.02f;
+		if( mTheta > 2*PI-0.01f)	mTheta = .02f;
+
+		if( laserTheta < 0.01f )	laserTheta = 2*PI-.02f;
+		if( laserTheta > 2*PI-0.01f)	laserTheta = .02f;
+	
+		// Convert Spherical to Cartesian coordinates: mPhi measured from +y
+		// and mTheta measured counterclockwise from -z.
+		mEyePos.x =  mRadius*sinf(mPhi)*sinf(-mTheta);
+		mEyePos.z = -mRadius*sinf(mPhi)*cosf(-mTheta);
+		mEyePos.y =  mRadius*cosf(mPhi);
+	
+		// Build the view matrix.
+		D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+		D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+		D3DXMatrixLookAtLH(&mView, &mEyePos, &target, &up);
+
+		break;
 	}
 
+	case end:
+		//UPDATE CUBE FOR ENDING SPLASHSCREEN
+		/*if(GetAsyncKeyState(VK_RETURN) & 0x8000) 
+		{
+			reinitialize();
+			state = game;
+		}*/
+		//PLAY SOUND
+		break;
 
-	
-	// Restrict the angle mPhi.
-	if( mPhi < 0.1f )	mPhi = 0.1f;
-	if( mPhi > PI-0.1f)	mPhi = PI-0.1f;
+	}
 
-	if( mTheta < 0.01f )	mTheta = 2*PI-.02f;
-	if( mTheta > 2*PI-0.01f)	mTheta = .02f;
-
-	if( laserTheta < 0.01f )	laserTheta = 2*PI-.02f;
-	if( laserTheta > 2*PI-0.01f)	laserTheta = .02f;
-	
-	// Convert Spherical to Cartesian coordinates: mPhi measured from +y
-	// and mTheta measured counterclockwise from -z.
-	mEyePos.x =  mRadius*sinf(mPhi)*sinf(-mTheta);
-	mEyePos.z = -mRadius*sinf(mPhi)*cosf(-mTheta);
-	mEyePos.y =  mRadius*cosf(mPhi);
-	
-	// Build the view matrix.
-	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
-	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
-	D3DXMatrixLookAtLH(&mView, &mEyePos, &target, &up);
 }
 
 void CrateApp::regenerateWalls(float dt)
@@ -480,35 +564,6 @@ void CrateApp::drawScene()
 		//mCrateMesh.draw();
     }
 
-	//BOSS
-	/*mWVP = boss.getWorldMatrix() *mView*mProj;
-	mfxWVPVar->SetMatrix((float*)&mWVP);
-	boss.setMTech(mTech);
-	boss.draw();*/
-	mWVP = mesh.getWorldMatrix() * mView * mProj;
-	mfxWVPVar->SetMatrix((float*)&mWVP);
-	mesh.setMTech(mTech);
-	mesh.draw();
-
-	//Matrix s;
-	//RotateY(&s, PI/4);
-
-
-
-	//HEALTH BAR:
-	Matrix rphi, rtheta;
-	RotateX(&rphi, -mPhi);
-	RotateY(&rtheta, -mTheta);
-
-	/*for(int i=0; i<1; i++)
-	{
-		mWVP = health[i].getWorldMatrix() * rphi * rtheta * mView*mProj;
-		mfxWVPVar->SetMatrix((float*)&mWVP);
-		health[i].setMTech(mTech);
-		health[i].draw();
-	}*/
-
-
 	//BACKGROUND
 	mfxDiffuseMapVar->SetResource(mBackgroundRV);
 	mfxSpecMapVar->SetResource(mSpecMapRV);
@@ -519,72 +574,121 @@ void CrateApp::drawScene()
 		background[i].draw();
 	}
 
-	Matrix s;
-	RotateY(&s, PI/4);
+	switch(state) {
 
-	//LAYERS:
-	mfxDiffuseMapVar->SetResource(mDiffuseMapRV);
-	mfxSpecMapVar->SetResource(mSpecMapRV);
-	for(int i=0; i<3; i++)
-	{
-		for(int j=0; j<NUM_WALLS; j++)
+	case start:
 		{
-			mWVP = layers[i].walls[j].getWorldMatrix() * layers[i].translate  * layers[i].rotations[j] * layers[i].spin * mView*mProj;
-			mfxWVPVar->SetMatrix((float*)&mWVP);
-			layers[i].walls[j].setMTech(mTech);
-			layers[i].walls[j].draw();
-		}
-	}
+			//DRAW INTRO SCREEN CUBE
 
-	//FOR DIAGONAL ROTATIONS:
-	for(int i=3; i<NUM_LAYERS; i++)
-	{
-		for(int j=0; j<NUM_WALLS; j++)
-		{
-			mWVP = layers[i].walls[j].getWorldMatrix() * layers[i].translate  * layers[i].rotations[j] * layers[i].spin * layers[i].diagonal * mView*mProj;
-			mfxWVPVar->SetMatrix((float*)&mWVP);
-			layers[i].walls[j].setMTech(mTech);
-			layers[i].walls[j].draw();
 		}
-	}
+
+	case game:
+		{
+			//BOSS
+			/*mWVP = boss.getWorldMatrix() *mView*mProj;
+			mfxWVPVar->SetMatrix((float*)&mWVP);
+			boss.setMTech(mTech);
+			boss.draw();*/
+			mWVP = mesh.getWorldMatrix() * mView * mProj;
+			mfxWVPVar->SetMatrix((float*)&mWVP);
+			mesh.setMTech(mTech);
+			mesh.draw();
+
+			//Matrix s;
+			//RotateY(&s, PI/4);
+
+
+
+			//HEALTH BAR:
+			Matrix rphi, rtheta;
+			RotateX(&rphi, -mPhi);
+			RotateY(&rtheta, -mTheta);
+
+			/*for(int i=0; i<1; i++)
+			{
+				mWVP = health[i].getWorldMatrix() * rphi * rtheta * mView*mProj;
+				mfxWVPVar->SetMatrix((float*)&mWVP);
+				health[i].setMTech(mTech);
+				health[i].draw();
+			}*/
+
 
 	
-	//bullet
-	bulletObject.setMTech(mTech);
-	bulletObject.draw(mfxWVPVar, mView*mProj);
+
+			Matrix s;
+			RotateY(&s, PI/4);
+
+			//LAYERS:
+			mfxDiffuseMapVar->SetResource(mDiffuseMapRV);
+			mfxSpecMapVar->SetResource(mSpecMapRV);
+			for(int i=0; i<3; i++)
+			{
+				for(int j=0; j<NUM_WALLS; j++)
+				{
+					mWVP = layers[i].walls[j].getWorldMatrix() * layers[i].translate  * layers[i].rotations[j] * layers[i].spin * mView*mProj;
+					mfxWVPVar->SetMatrix((float*)&mWVP);
+					layers[i].walls[j].setMTech(mTech);
+					layers[i].walls[j].draw();
+				}
+			}
+
+			//FOR DIAGONAL ROTATIONS:
+			for(int i=3; i<NUM_LAYERS; i++)
+			{
+				for(int j=0; j<NUM_WALLS; j++)
+				{
+					mWVP = layers[i].walls[j].getWorldMatrix() * layers[i].translate  * layers[i].rotations[j] * layers[i].spin * layers[i].diagonal * mView*mProj;
+					mfxWVPVar->SetMatrix((float*)&mWVP);
+					layers[i].walls[j].setMTech(mTech);
+					layers[i].walls[j].draw();
+				}
+			}
+
 	
-	//laser
-	//TRACKING:
-	if(abs(laserTheta-mTheta)>.01)
-	{
-		if((laserTheta-mTheta > -PI && laserTheta-mTheta < 0) || (laserTheta-mTheta > PI)) laserTheta += .0015f;
-		else laserTheta -= .0015f;
-	}
-	if(abs(laserPhi-mPhi)>.01)
-	{
-		if(laserPhi > mPhi) laserPhi -= .0015f;
-		else laserPhi += .0015f;
-	}
-	Matrix translateOut;
-	Matrix rotatePhi, rotateTheta;
-	RotateX(&rotatePhi, -laserPhi+PI/2);
-	RotateY(&rotateTheta, laserTheta);
-	Translate(&translateOut, 0, 0, -mRadius*2);
-	mWVP = laser.getWorldMatrix() *translateOut * rotatePhi * rotateTheta * mView*mProj;
-	mfxWVPVar->SetMatrix((float*)&mWVP);
-	laser.setMTech(mTech);
-	laser.draw();
+			//bullet
+			bulletObject.setMTech(mTech);
+			bulletObject.draw(mfxWVPVar, mView*mProj);
+	
+			//laser
+			//TRACKING:
+			if(abs(laserTheta-mTheta)>.01)
+			{
+				if((laserTheta-mTheta > -PI && laserTheta-mTheta < 0) || (laserTheta-mTheta > PI)) laserTheta += .0015f;
+				else laserTheta -= .0015f;
+			}
+			if(abs(laserPhi-mPhi)>.01)
+			{
+				if(laserPhi > mPhi) laserPhi -= .0015f;
+				else laserPhi += .0015f;
+			}
+			Matrix translateOut;
+			Matrix rotatePhi, rotateTheta;
+			RotateX(&rotatePhi, -laserPhi+PI/2);
+			RotateY(&rotateTheta, laserTheta);
+			Translate(&translateOut, 0, 0, -mRadius*2);
+			mWVP = laser.getWorldMatrix() *translateOut * rotatePhi * rotateTheta * mView*mProj;
+			mfxWVPVar->SetMatrix((float*)&mWVP);
+			laser.setMTech(mTech);
+			laser.draw();
 
-	std::wostringstream outs;   
-	outs.precision(2);
-	//outs << L"Phi: " <<  mPhi << "\nTheta: " << mTheta << "\n\n laser phi: " << laserPhi << "\nlaser theta: " << laserTheta
-	outs << L"Phi: " <<  mPhi << "\nTheta: " << mTheta << "\n\n laser phi: " << laserPhi << "\nlaser theta: " << laserTheta << "\nwall theta: " << layers[3].thetas[0] << "\nwall phi: " << layers[3].phis[0];;
-	stats = outs.str();
-	// We specify DT_NOCLIP, so we do not care about width/height of the rect.
-	RECT R = {5, 5, 0, 0};
-	mFont->DrawText(0, stats.c_str(), -1, &R, DT_NOCLIP, BLACK);
+			std::wostringstream outs;   
+			outs.precision(2);
+			//outs << L"Phi: " <<  mPhi << "\nTheta: " << mTheta << "\n\n laser phi: " << laserPhi << "\nlaser theta: " << laserTheta
+			outs << L"Phi: " <<  mPhi << "\nTheta: " << mTheta << "\n\n laser phi: " << laserPhi << "\nlaser theta: " << laserTheta << "\nwall theta: " << layers[3].thetas[0] << "\nwall phi: " << layers[3].phis[0];;
+			stats = outs.str();
+			// We specify DT_NOCLIP, so we do not care about width/height of the rect.
+			RECT R = {5, 5, 0, 0};
+			mFont->DrawText(0, stats.c_str(), -1, &R, DT_NOCLIP, BLACK);
 
-	mSwapChain->Present(0, 0);
+			mSwapChain->Present(0, 0);
+		}
+	case end:
+		//DRAW END SCREEN CUBE
+		break;
+	}
+
+
+	
 }
 
 void CrateApp::buildFX()
